a50bec8efe662c934bae2321f5837358d1fcb5a6
Nasser Alomri <nasser.omer133@gmail.com>
2024-11-13 00:27:44 +0300

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -1,9 +1,7 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -27,4 +25,3 @@ public class TestHello {
         }
     }
 }
-
